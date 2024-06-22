fd1c8f8c87a8ff559bb633c681c0bddfcdd598b6
Neeraj <suthar.nrj@gmail.com>
2024-06-22 12:32:29 +0530

    Init

---
+++ src/main/java/com/devjam/notify/CcNotifierApplication.java
@@ -0,0 +1,13 @@
+package com.devjam.notify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CcNotifierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CcNotifierApplication.class, args);
+	}
+
+}
--- src/test/java/com/devjam/notify/CcNotifierApplicationTests.java
+++ src/test/java/com/devjam/notify/CcNotifierApplicationTests.java
@@ -1,13 +1,13 @@
-package com.devjam.notifier;
+package com.devjam.notify;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class NotifierApplicationTests {
+class CcNotifierApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
